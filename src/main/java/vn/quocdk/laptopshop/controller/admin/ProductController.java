1790e67844291c525e123d5a95dae8ae99a2e55c
HoangNam <lenam7546@gmail.com>
2025-02-16 15:02:02 +0700

    ok

--- src/main/java/vn/quocdk/laptopshop/controller/admin/ProductController.java
+++ src/main/java/vn/quocdk/laptopshop/controller/admin/ProductController.java
@@ -51,6 +51,11 @@ public class ProductController {
         Product product = productService.getProductById(id);
         model.addAttribute("product", product);
         return "admin/product/detail";
+    } @GetMapping("/admin/product/{id}")
+    public String getProductDetailPage2(@PathVariable long id, Model model) {
+        Product product = productService.getProductById(id);
+        model.addAttribute("product", product);
+        return "admin/product/detail";
     }
 
     @GetMapping("/admin/product/create")
